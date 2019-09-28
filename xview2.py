5d62d28e808a598e5de80457fa57924fe972734f
ash1995 <ash1995@gmail.com>
2019-09-28 21:19:33 +0400

    Modify functions to work with dataframe

--- xview2.py
+++ xview2.py
@@ -13,6 +13,13 @@ import utils
 
 class XView2():
     def __init__(self, img_dir, lbl_dir):
+        """
+        Constructor of xView helper class for reading, parsing & visualising annotations.
+
+        :param image_folder (str): location to the folder that hosts images.
+        :param annotation_file (str): location to the folder that hosts annotations.
+        :return:
+        """
         self.img_dir = img_dir
         self.lbl_dir = lbl_dir
 
@@ -23,7 +30,7 @@ class XView2():
         print('Loading annotations into memory...')
         tic = time.time()
         self.anns = [json.load(open(ann, 'r')) for ann in self.jsons]
-        print('Done (t={:0.2f}s)'.format(time.time()- tic))
+        print('Done (t={:0.2f}s)'.format(time.time() - tic))
 
         # Create annotation dictionary
         self.anndict = dict(zip(self.jsons, self.anns))
@@ -31,44 +38,14 @@ class XView2():
         print('Creating annotation dataframe...')
         tic = time.time()
         self.anndf = self.generate_dataframe()
-        print('Done (t={:0.2f}s)'.format(time.time()- tic))
-
-    def get_object_polygons(self, ann, _type="wkt"):
-        p = self.anndict[ann]
-        wktlist = [obj['wkt'] for obj in p['features']['xy']]
-        if os.path.basename(ann).split('_')[2] == "post":
-            dmg = [obj['properties']['subtype'] for obj in p['features']['xy']]
-        else:
-            dmg = None
-        polylist = utils.wkt2list(wktlist)
-        if _type == "wkt":
-            return wktlist, dmg
-        elif _type == "poly":
-            return polylist, dmg
-
-    def get_metadata(self, ann):
-        return self.anndict[ann]['metadata']
-
-    def pre_post_split(self):
-        # Separate into pre and post
-        self.pre_imgs = sorted([os.path.join(self.img_dir, im) for im in os.listdir(self.img_dir) if im.split('_')[2] == "pre"])
-        self.pre_lbls = sorted([os.path.join(self.lbl_dir, lbl) for lbl in os.listdir(self.lbl_dir) if lbl.split('_')[2] == "pre"])
-        self.post_imgs = sorted([os.path.join(self.img_dir, im) for im in os.listdir(self.img_dir) if im.split('_')[2] == "post"])
-        self.post_lbls = sorted([os.path.join(self.lbl_dir, lbl) for lbl in os.listdir(self.lbl_dir) if lbl.split('_')[2] == "post"])
-        assert len(self.pre_imgs) == len(self.post_imgs)
-        assert len(self.pre_lbls) == len(self.post_lbls)
-        # Load annotations into memory
-        print('Loading annotations into memory...')
-        tic = time.time()
-        self.pre_anns = [json.load(open(ann, 'r')) for ann in self.pre_lbls]
-        self.post_anns = [json.load(open(ann, 'r')) for ann in self.post_lbls]
         print('Done (t={:0.2f}s)'.format(time.time() - tic))
 
-        # Create annotation dictionary
-        self.pre_ann = dict(zip(self.pre_lbls, self.pre_anns))
-        self.post_ann = dict(zip(self.post_lbls, self.post_anns))
-
     def generate_dataframe(self):
+        """
+        Generate main annotation dataframe.
+
+        :return: anndf (pandas DataFrame)
+        """
         ann_list = []
         for fname, ann in self.anndict.items():
             # Get features
@@ -101,9 +78,47 @@ class XView2():
         anndf = pd.concat(ann_list, ignore_index=True)
         return anndf
 
+    def get_object_polygons(self, ann, wkt_type="pixel", _type="wkt"):
+        """
+        Return object polygons & damage status
+
+        :param ann (str): location of annotation file to parse
+        :param wkt_type (str): wkt version to be returned (pixel or geo)
+        :param _type (str): format of polygons to be returned (wkt or list of lists)
+        :return: None
+        """
+        fname = os.path.basename(ann)[:-5]
+        img_name = fname + ".png"
+        ann = self.anndf.loc[self.anndf["img_name"] == img_name]
+        if wkt_type == "pixel":
+            wktlist = ann['pixwkt'].tolist()
+        elif wkt_type == "geo":
+            wktlist = ann['geowkt'].tolist()
+        dmg = ann['dmg_cat'].tolist()
+        polylist = utils.wkt2list(wktlist)
+        if _type == "wkt":
+            return wktlist, dmg
+        elif _type == "poly":
+            return polylist, dmg
+
+    def pre_post_split(self):
+        """
+        Generate pre-disaster and post-disaster dataframes from main dataframe.
+
+        :return: None
+        """
+        self.pre_dist_df = self.anndf.loc[self.anndf["dmg_cat"] == 'none']
+        self.post_dist_df = self.anndf.loc[self.anndf["dmg_cat"] != 'none']
+
     def show_anns(self, ann):
-        metadata = self.get_metadata(ann)
-        img_name = metadata['img_name']
+        """
+        Display the specified annotations.
+
+        :param ann (str): location of annotation file to display
+        :return: None
+        """
+        fname = os.path.basename(ann)[:-5]
+        img_name = fname + ".png"
         img_path = os.path.join(self.img_dir, img_name)
         imgfile = plt.imread(img_path)
 
@@ -111,7 +126,7 @@ class XView2():
         plist, dmg = self.get_object_polygons(ann, _type="poly")
         print("Number of objects =", len(plist))
 
-        plt.figure(figsize=(15,15))
+        plt.figure(figsize=(15, 15))
         ax = plt.gca()
         polygons = []
         colordict = {'no-damage': 'w',
@@ -122,25 +137,37 @@ class XView2():
         color = []
         if len(plist) != 0:
             # Pre_disaster Images
-            if dmg == None:
+            if dmg is None:
                 for p in plist:
                     c = (np.random.random((1, 3)) * 0.6 + 0.4).tolist()[0]
                     polygons.append(Polygon(p[0]))
                     color.append(c)
-                    p = PatchCollection(polygons, facecolor=color, linewidths=0, alpha=0.4)
+                    p = PatchCollection(polygons,
+                                        facecolor=color,
+                                        linewidths=0,
+                                        alpha=0.4)
                 ax.add_collection(p)
-                p = PatchCollection(polygons, edgecolors=color, facecolor='none', linewidths=2)
+                p = PatchCollection(polygons,
+                                    edgecolors=color,
+                                    facecolor='none',
+                                    linewidths=2)
                 ax.add_collection(p)
 
             # Post_disaster Images
             else:
-                for p, d in zip(plist,dmg):
+                for p, d in zip(plist, dmg):
                     c = colordict[d]
                     polygons.append(Polygon(p[0]))
                     color.append(c)
-                    p = PatchCollection(polygons, facecolor=color, linewidths=0, alpha=0.4)
+                    p = PatchCollection(polygons,
+                                        facecolor=color,
+                                        linewidths=0,
+                                        alpha=0.4)
                 ax.add_collection(p)
-                p = PatchCollection(polygons, edgecolors=color, facecolor='none', linewidths=2)
+                p = PatchCollection(polygons,
+                                    edgecolors=color,
+                                    facecolor='none',
+                                    linewidths=2)
                 ax.add_collection(p)
         ax.axis('off')
         ax.set_title(os.path.basename(ann[:-5]))
@@ -155,3 +182,4 @@ if __name__ == "__main__":
     lbl_dir = os.path.join(data_dir, folder, 'labels')
     xview = XView2(img_dir, lbl_dir)
     xview.show_anns('./data/train/labels/palu-tsunami_00000002_post_disaster.json')
+    print(xview.anndf.head())
